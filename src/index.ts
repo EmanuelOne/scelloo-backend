0c76d07271722cd0c6b2eb56142de102cfed80ff
emanuelone <emanuel4mechie@gmail.com>
2022-05-16 12:01:22 +0100

    resolve SSL error from postgres

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import * as cors from 'cors';
 import cartRoute from './routes/cart.routes';
 import { DB } from './data-source';
 import couponRoute from './routes/coupon.routes';
+import * as dotenv from 'dotenv';
+dotenv.config();
 DB.initialize()
   .then(async () => {
+    const port = process.env.PORT || 3000;
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors());
     app.use('/cart', cartRoute);
     app.use('/coupon', couponRoute);
-    app.listen(3000, () => {
-      console.log('Express server started on port 3000');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((error) => console.log(error));
